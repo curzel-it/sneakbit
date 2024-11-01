c9d11f33615beca258ec757621e9b79525938b15
Federico Curzel <federico@curzel.it>
2024-11-01 23:39:13 +0100

    Now using executable path as root

--- game/src/main.rs
+++ game/src/main.rs
@@ -192,7 +192,16 @@ fn rendering_scale_for_screen_width(width: f32) -> (f32, f32) {
 }
 
 fn local_path(filename: &str) -> PathBuf {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let cargo_manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let exe_path = env::current_exe().unwrap_or(cargo_manifest_path);
+    let exe_str = exe_path.to_str().unwrap();
+    let base = exe_str
+        .replace("target/debug/game", "game")
+        .replace("target/release/game", "game")
+        .replace("target\\debug\\game", "game")
+        .replace("target\\release\\game", "game");
+
+    let mut path = PathBuf::from(base);
     path.push("..");
     path.push(filename);
     path
