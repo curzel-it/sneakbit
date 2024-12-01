544ec175ece904157860769eb9cb2f084fa3b3ba
Federico Curzel <federico@curzel.it>
2024-12-01 02:32:03 +0100

    Just saving work

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "it.curzel.bitscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.2.6"
+        versionCode = 31
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
