5b875f8c455067d1b1cf0995ee45afc348cea66d
Federico Curzel <federico@curzel.it>
2024-12-16 01:59:45 +0100

    Moved audio to a dedicated thread

--- game/src/features/audio.rs
+++ game/src/features/audio.rs
@@ -1,120 +1,147 @@
-use common_macros::hash_map;
 use raylib::prelude::*;
 
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf, sync::mpsc::{self, Sender}, thread};
 
 use game_core::{current_sound_effects, current_soundtrack_string, features::sound_effects::{are_sound_effects_enabled, is_music_enabled, SoundEffect}};
 
+use crate::GameContext;
+
 use super::paths::root_path;
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum AppSound {
     Effect(SoundEffect),
-    Track(String)
+    Track(String),
 }
 
-pub struct SoundContext<'a> {
-    pub music_was_enabled: bool,
-    pub sound_library: HashMap<AppSound, Sound<'a>>
+pub enum AudioCommand {
+    PlaySoundEffect(SoundEffect),
+    PlayMusic(String),
+    StopMusic,
 }
 
-pub fn load_sounds(rl: &mut Result<raylib::prelude::RaylibAudio, RaylibAudioInitError>) -> HashMap<AppSound, Sound> {
-    if let Ok(rl) = rl {
-        vec![
-            (AppSound::Effect(SoundEffect::DeathOfNonMonster), "sfx_deathscream_android7.mp3"),
-            (AppSound::Effect(SoundEffect::DeathOfMonster), "sfx_deathscream_human11.mp3"),
-            (AppSound::Effect(SoundEffect::SmallExplosion), "sfx_exp_short_hard8.mp3"),
-            (AppSound::Effect(SoundEffect::WorldChange), "sfx_movement_dooropen1.mp3"),
-            (AppSound::Effect(SoundEffect::StepTaken), "sfx_movement_footsteps1a.mp3"),
-            (AppSound::Effect(SoundEffect::KnifeThrown), "sfx_movement_jump12_landing.mp3"),
-            (AppSound::Effect(SoundEffect::BulletBounced), "sfx_movement_jump20.mp3"),
-            (AppSound::Effect(SoundEffect::HintReceived), "sfx_sound_neutral5.mp3"),
-            (AppSound::Effect(SoundEffect::KeyCollected), "sfx_sounds_fanfare3.mp3"),
-            (AppSound::Effect(SoundEffect::AmmoCollected), "sfx_sounds_interaction22.mp3"),
-            (AppSound::Effect(SoundEffect::GameOver), "sfx_sounds_negative1.mp3"),
-            (AppSound::Effect(SoundEffect::PlayerResurrected), "sfx_sounds_powerup1.mp3"), 
-            (AppSound::Effect(SoundEffect::NoAmmo), "sfx_wpn_noammo3.mp3"),
-            (AppSound::Effect(SoundEffect::SwordSlash), "sfx_wpn_sword2.mp3"),
-            (AppSound::Effect(SoundEffect::GunShot), "sfx_wpn_machinegun_loop1.mp3"),
-            (AppSound::Effect(SoundEffect::LoudGunShot), "sfx_weapon_shotgun3.mp3"),
-            track_track_pair("pol_brave_worm_short.mp3"),
-            track_track_pair("pol_cactus_land_short.mp3"),
-            track_track_pair("pol_chubby_cat_short.mp3"),
-            track_track_pair("pol_clouds_castle_short.mp3"),
-            track_track_pair("pol_combat_plan_short.mp3"),
-            track_track_pair("pol_flash_run_short.mp3"),
-            track_track_pair("pol_king_of_coins_short.mp3"),
-            track_track_pair("pol_magical_sun_short.mp3"),
-            track_track_pair("pol_nuts_and_bolts_short.mp3"),
-            track_track_pair("pol_palm_beach_short.mp3"),
-            track_track_pair("pol_pyramid_sands_short.mp3"),
-            track_track_pair("pol_spirits_dance_short.mp3"),
-            track_track_pair("pol_the_dojo_short.mp3"),
-            track_track_pair("pol_final_sacrifice_short.mp3"),
-            track_track_pair("pol_code_geek_short.mp3"),
+pub struct AudioManager {
+    sender: Sender<AudioCommand>,
+}
 
-        ]
-        .into_iter()
-        .filter_map(|(effect, filename)| {
-            if let Some(path) = audio_path_for_filename(filename).as_os_str().to_str() {
-                if let Ok(mut sound) = rl.new_sound(path) {
-                    sound.set_volume(volume_for_sound_effect(&effect));
-                    return Some((effect, sound))
+pub fn play_audio(context: &GameContext) {
+    play_sound_effects(&context);
+    play_music(&context);
+}
+
+impl AudioManager {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel();
+
+        thread::spawn(move || {
+            let mut rl_audio = match RaylibAudio::init_audio_device() {
+                Ok(audio) => audio,
+                Err(e) => {
+                    eprintln!("Failed to initialize audio device: {:?}", e);
+                    return;
+                }
+            };
+
+            let mut sound_library = load_sounds(&mut rl_audio);
+            let mut current_track: Option<AppSound> = None;
+
+            for command in receiver {
+                match command {
+                    AudioCommand::PlaySoundEffect(effect) => {
+                        let key = AppSound::Effect(effect);
+                        if let Some(sound) = sound_library.get(&key) {
+                            sound.play();
+                        }
+                    }
+                    AudioCommand::PlayMusic(track_name) => {
+                        let key = AppSound::Track(track_name.clone());
+
+                        if current_track.as_ref() != Some(&key) {
+                            stop_music(&mut sound_library);
+                        }
+                        if let Some(sound) = sound_library.get(&key) {
+                            if !sound.is_playing() {
+                                sound.play();
+                            }
+                            current_track = Some(key);
+                        }
+                    }
+                    AudioCommand::StopMusic => {
+                        stop_music(&mut sound_library);
+                        current_track = None;
+                    }
                 }
             }
-            None
-        })
-        .collect()
-    } else {
-        hash_map!()
+        });
+
+        AudioManager { sender }
+    }
+
+    pub fn play_sound_effect(&self, effect: SoundEffect) {
+        let _ = self.sender.send(AudioCommand::PlaySoundEffect(effect));
+    }
+
+    pub fn play_music(&self, track_name: String) {
+        let _ = self.sender.send(AudioCommand::PlayMusic(track_name));
+    }
+
+    pub fn stop_music(&self) {
+        let _ = self.sender.send(AudioCommand::StopMusic);
     }
 }
 
-pub fn play_sound_effects(context: &SoundContext) {
-    let sound_effects = current_sound_effects();
-    if sound_effects.is_empty() {
-        return
-    }
-    if !are_sound_effects_enabled() {
-        return
-    }
-    sound_effects.iter().for_each(|effect| {
-        let key = &AppSound::Effect(effect.clone());
-        if let Some(sound) = context.sound_library.get(key) {
-            sound.play();
-        }
-    })
-}
+fn load_sounds(rl_audio: &mut RaylibAudio) -> HashMap<AppSound, Sound> {
+    let sound_filenames = vec![
+        (AppSound::Effect(SoundEffect::DeathOfNonMonster), "sfx_deathscream_android7.mp3"),
+        (AppSound::Effect(SoundEffect::DeathOfMonster), "sfx_deathscream_human11.mp3"),
+        (AppSound::Effect(SoundEffect::SmallExplosion), "sfx_exp_short_hard8.mp3"),
+        (AppSound::Effect(SoundEffect::WorldChange), "sfx_movement_dooropen1.mp3"),
+        (AppSound::Effect(SoundEffect::StepTaken), "sfx_movement_footsteps1a.mp3"),
+        (AppSound::Effect(SoundEffect::KnifeThrown), "sfx_movement_jump12_landing.mp3"),
+        (AppSound::Effect(SoundEffect::BulletBounced), "sfx_movement_jump20.mp3"),
+        (AppSound::Effect(SoundEffect::HintReceived), "sfx_sound_neutral5.mp3"),
+        (AppSound::Effect(SoundEffect::KeyCollected), "sfx_sounds_fanfare3.mp3"),
+        (AppSound::Effect(SoundEffect::AmmoCollected), "sfx_sounds_interaction22.mp3"),
+        (AppSound::Effect(SoundEffect::GameOver), "sfx_sounds_negative1.mp3"),
+        (AppSound::Effect(SoundEffect::PlayerResurrected), "sfx_sounds_powerup1.mp3"),
+        (AppSound::Effect(SoundEffect::NoAmmo), "sfx_wpn_noammo3.mp3"),
+        (AppSound::Effect(SoundEffect::SwordSlash), "sfx_wpn_sword2.mp3"),
+        (AppSound::Effect(SoundEffect::GunShot), "sfx_wpn_machinegun_loop1.mp3"),
+        (AppSound::Effect(SoundEffect::LoudGunShot), "sfx_weapon_shotgun3.mp3"),
+        track_track_pair("pol_brave_worm_short.mp3"),
+        track_track_pair("pol_cactus_land_short.mp3"),
+        track_track_pair("pol_chubby_cat_short.mp3"),
+        track_track_pair("pol_clouds_castle_short.mp3"),
+        track_track_pair("pol_combat_plan_short.mp3"),
+        track_track_pair("pol_flash_run_short.mp3"),
+        track_track_pair("pol_king_of_coins_short.mp3"),
+        track_track_pair("pol_magical_sun_short.mp3"),
+        track_track_pair("pol_nuts_and_bolts_short.mp3"),
+        track_track_pair("pol_palm_beach_short.mp3"),
+        track_track_pair("pol_pyramid_sands_short.mp3"),
+        track_track_pair("pol_spirits_dance_short.mp3"),
+        track_track_pair("pol_the_dojo_short.mp3"),
+        track_track_pair("pol_final_sacrifice_short.mp3"),
+        track_track_pair("pol_code_geek_short.mp3"),
+    ];
 
-pub fn play_music(context: &mut SoundContext) {
-    if is_music_enabled() {
-        context.music_was_enabled = true;
-        update_sound_track(context);
-    } else if context.music_was_enabled {
-        context.music_was_enabled = false;
-        stop_music(context);
-    }
-}
+    let mut sound_library = HashMap::new();
 
-pub fn update_sound_track(context: &mut SoundContext) {
-    if let Some(track_name) = current_soundtrack_string() {
-        if !track_name.is_empty() {
-            
-            let key = &AppSound::Track(track_name);
-            
-            if let Some(sound) = context.sound_library.get(key) {
-                if !sound.is_playing() {
-                    let _ = sound;
-                    stop_music(context);
-                }
-            }            
-            if let Some(sound) = context.sound_library.get(key) {
-                if !sound.is_playing() {
-                    sound.play();
-                }
+    for (app_sound, filename) in sound_filenames {
+        let path = audio_path_for_filename(filename);
+        if let Some(path_str) = path.to_str() {
+            if let Ok(mut sound) = rl_audio.new_sound(path_str) {
+                sound.set_volume(volume_for_sound_effect(&app_sound));
+                sound_library.insert(app_sound, sound);
+            } else {
+                eprintln!("Failed to load sound: {}", filename);
             }
+        } else {
+            eprintln!("Invalid path for sound: {}", filename);
         }
     }
+
+    sound_library
 }
 
 fn track_track_pair(filename: &str) -> (AppSound, &str) {
@@ -131,9 +158,9 @@ fn volume_for_sound_effect(sound: &AppSound) -> f32 {
             SoundEffect::BulletBounced => 0.2,
             SoundEffect::WorldChange => 0.7,
             SoundEffect::AmmoCollected => 0.6,
-            _ => 0.8
+            _ => 0.8,
         },
-        AppSound::Track(_) => 0.3
+        AppSound::Track(_) => 0.3,
     }
 }
 
@@ -144,16 +171,40 @@ fn audio_path_for_filename(filename: &str) -> PathBuf {
     path
 }
 
-fn stop_music(context: &mut SoundContext) {
-    let sounds: Vec<AppSound> = context.sound_library.keys().cloned().collect();
-
-    sounds.iter().for_each(|key| {
-        if matches!(key, AppSound::Track(_)) {
-            if let Some(sound) = context.sound_library.get_mut(key) {
+fn stop_music<'a>(sound_library: &mut HashMap<AppSound, Sound<'a>>) {
+    let keys: Vec<AppSound> = sound_library.keys().cloned().collect();
+    for key in keys {
+        if let AppSound::Track(_) = key {
+            if let Some(sound) = sound_library.get_mut(&key) {
                 if sound.is_playing() {
                     sound.stop();
                 }
             }
         }
-    });
+    }
+}
+
+fn play_sound_effects(context: &GameContext) {
+    let sound_effects = current_sound_effects();
+    if sound_effects.is_empty() {
+        return;
+    }
+    if !are_sound_effects_enabled() {
+        return;
+    }
+    for effect in sound_effects.iter() {
+        context.audio_manager.play_sound_effect(effect.clone());
+    }
+}
+
+fn play_music(context: &GameContext) {
+    if is_music_enabled() {
+        if let Some(track_name) = current_soundtrack_string() {
+            if !track_name.is_empty() {
+                context.audio_manager.play_music(track_name.clone());
+            }
+        }
+    } else {
+        context.audio_manager.stop_music();
+    }
 }
--- game/src/main.rs
+++ game/src/main.rs
@@ -5,8 +5,8 @@ mod rendering;
 
 use std::env;
 
-use features::{audio::{load_sounds, play_music, play_sound_effects, update_sound_track, SoundContext}, inputs::{handle_keyboard_updates, handle_mouse_updates}, links::MyLinkHandler, paths::local_path};
-use game_core::{config::initialize_config_paths, constants::TILE_SIZE, current_world_id, engine_set_wants_fullscreen, features::sound_effects::is_music_enabled, game_engine::{engine::GameMode, storage::{bool_for_global_key, StorageKey}}, initialize_game, is_game_running, lang::localizable::LANG_EN, set_links_handler, stop_game, update_game};
+use features::{audio::{play_audio, AudioManager}, inputs::{handle_keyboard_updates, handle_mouse_updates}, links::MyLinkHandler, paths::local_path};
+use game_core::{config::initialize_config_paths, constants::TILE_SIZE, current_soundtrack_string, current_world_id, engine_set_wants_fullscreen, features::sound_effects::is_music_enabled, game_engine::{engine::GameMode, storage::{bool_for_global_key, StorageKey}}, initialize_game, is_game_running, lang::localizable::LANG_EN, set_links_handler, stop_game, update_game};
 use raylib::prelude::*;
 use rendering::{textures::load_tile_map_textures, ui::get_rendering_config, window::{handle_window_updates, render_frame_with_context, start_rl}};
 use sys_locale::get_locale;
@@ -20,7 +20,8 @@ struct GameContext {
     total_run_time: f32,
     using_controller: bool,
     last_number_of_players: usize,
-    last_pvp: bool
+    last_pvp: bool,
+    audio_manager: AudioManager, 
 }
 
 fn main() {
@@ -31,17 +32,13 @@ fn main() {
         local_path("data"),
         local_path("data/species.json"),
         local_path("data/save.json"),
-        local_path("lang")
+        local_path("lang"),
     );
 
-    let creative_mode = env::args().any(|arg| arg == "creative");    
-    let (rl, rl_thread) = start_rl(creative_mode);    
+    let creative_mode = env::args().any(|arg| arg == "creative");
+    let (rl, rl_thread) = start_rl(creative_mode);
 
-    let mut rl_audio = start_rl_audio();    
-    let mut sound_context = SoundContext {
-        music_was_enabled: true,
-        sound_library: load_sounds(&mut rl_audio)
-    };
+    let audio_manager = AudioManager::new();
 
     let mut context = GameContext {
         rl,
@@ -53,11 +50,18 @@ fn main() {
         using_controller: false,
         last_number_of_players: 1,
         last_pvp: false,
+        audio_manager,
     };
 
-    initialize_game(if creative_mode { GameMode::Creative } else { GameMode::RealTimeCoOp });    
+    let initial_game_mode = if creative_mode {
+        GameMode::Creative
+    } else {
+        GameMode::RealTimeCoOp
+    };
+
+    initialize_game(initial_game_mode);
     set_links_handler(Box::new(MyLinkHandler {}));
-        
+
     if bool_for_global_key(&StorageKey::fullscreen()) {
         engine_set_wants_fullscreen();
     }
@@ -71,33 +75,35 @@ fn main() {
         handle_keyboard_updates(&mut context, time_since_last_update);
         handle_mouse_updates(&mut context.rl, get_rendering_config().rendering_scale);
         update_game(time_since_last_update);
-        handle_world_changed(&mut context, &mut sound_context);
-        render_frame_with_context(&mut context);  
-        play_sound_effects(&sound_context);
-        play_music(&mut sound_context);
+        handle_world_changed(&mut context);
+        render_frame_with_context(&mut context);
+        play_audio(&context);
     }
 }
 
-fn handle_world_changed(context: &mut GameContext, sound_context: &mut SoundContext) {
-    let current_world = current_world_id(); 
+fn handle_world_changed(context: &mut GameContext) {
+    let current_world = current_world_id();
     if context.latest_world != current_world {
         context.latest_world = current_world;
         load_tile_map_textures(&mut context.rl, &context.rl_thread, current_world);
 
         if is_music_enabled() {
-            update_sound_track(sound_context);
+            if let Some(track_name) = current_soundtrack_string() {
+                if !track_name.is_empty() {
+                    context.audio_manager.play_music(track_name);
+                }
+            }
         }
     }
 }
 
 fn handle_game_closed(context: &mut GameContext) {
-    if context.rl.window_should_close() && !context.rl.is_key_pressed(raylib::consts::KeyboardKey::KEY_ESCAPE) {
+    if context.rl.window_should_close()
+        && !context.rl.is_key_pressed(raylib::consts::KeyboardKey::KEY_ESCAPE)
+    {
+        context.audio_manager.stop_music();
         stop_game();
     }
-}
-
-fn start_rl_audio() -> Result<raylib::prelude::RaylibAudio, RaylibAudioInitError> {
-    RaylibAudio::init_audio_device()
 }
 
 fn current_locale() -> String {
